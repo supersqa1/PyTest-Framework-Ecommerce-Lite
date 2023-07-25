4a5dcf5d577981da75720668ecdbd7d816592442
Admas Kinfu <supersoftwarequalityassurance@gmail.com>
2023-07-24 20:05:55 -0700

    updated code to use env variables for database name and table prefix

--- demostore_automation/src/dao/customers_dao.py
+++ demostore_automation/src/dao/customers_dao.py
@@ -10,7 +10,7 @@ class CustomersDAO:
         self.db_helper = DBUtility()
 
     def get_customer_by_email(self, email):
-        sql = f"""SELECT * FROM localdemostore.wp_users 
+        sql = f"""SELECT * FROM {self.db_helper.database}.{self.db_helper.table_prefix}users 
                   WHERE user_email = '{email}';"""
 
         rs_sql = self.db_helper.execute_select(sql)
@@ -18,7 +18,7 @@ class CustomersDAO:
         return rs_sql
 
     def get_random_customer_from_db(self, qty=1):
-        sql = f"SELECT user_email FROM localdemostore.wp_users order by id desc LIMIT 1000;"
+        sql = f"SELECT user_email FROM {self.db_helper.database}.{self.db_helper.table_prefix}users order by id desc LIMIT 1000;"
         rs_sql = self.db_helper.execute_select(sql)
         logger.info(f"Found {len(rs_sql)} random users from db.")
         return random.sample(rs_sql, int(qty))
