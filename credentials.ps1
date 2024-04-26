f2619f8800f59f1d131bd6997d393d81afb032ec
Admas Kinfu <supersoftwarequalityassurance@gmail.com>
2024-04-25 22:14:35 -0700

    added .ps1 variable files

---
+++ credentials.ps1
@@ -0,0 +1,5 @@
+$env:WOO_KEY = <your key>
+$env:WOO_SECRET = <your secret>
+
+$env:DB_USER = <your db user (if you are using MAMP this is 'root')>
+$env:DB_PASSWORD = <your db password (if you are using MAMP this is 'root')>
---
+++ variables_local.ps1
@@ -0,0 +1,16 @@
+# Setting environment variables in PowerShell for the session
+
+# URL setting
+$env:BASE_URL = "http://dev.bootcamp.store.supersqa.com/"
+
+# Browser setting
+$env:BROWSER = "chrome"
+
+# Database configurations
+$env:DB_PORT = "3309"
+$env:DB_HOST = "dev.bootcamp.store.supersqa.com"
+$env:DB_DATABASE = "demostore"
+$env:DB_TABLE_PREFIX = "wp_"
+
+# Python path setting
+#$env:PYTHONPATH = "C:\Users\YourUsername\Projects\teaching\QA-LIVE\frontiers\demostore-pytest-base"
